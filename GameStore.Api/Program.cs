bf7e1295a2826dfe2de1e5a70f6268245b92966b
DFBlok <s219392862@mandela.ac.za>
2024-11-08 09:41:36 +0200

    Dtos created

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,19 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<GameDtos> games = [
+    new (1, "Street Fighter II", "Fighting", 19.99M, new DateOnly(1999,7,15)),
+    new (2, "Final Fantasy XIV", "Roleplaying", 59.99M, new DateOnly(2010, 9, 30)),
+    new (3, "FIFA 23", "Sports", 69.99M, new DateOnly(2022, 9, 27))
+];
+//GET /GAMES
+app.MapGet("games", () => games);
 
+//get / games/1
+app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id));
+
+/* app.MapGet("/", () => "Hello World!");
+ */
 app.Run();
